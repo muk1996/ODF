64e42b49af94f8b1b18ebbbeb9504247f37aaca7
muk1996 <muhammad_khan96@hotmail.com>
2020-07-03 18:31:11 +0500

    updated setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,7 @@
-mkdir -p ~/.streamlit/
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-“ > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
