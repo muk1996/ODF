0e18a5f952856a1d72a2c6bf6dae22f6bbbd57e4
muk1996 <muhammad_khan96@hotmail.com>
2020-07-04 20:17:59 +0500

    add district

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pydeck as pdk
 import altair as alt
 from datetime import datetime
 from pandas import Series
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
 
 st.title("Open Data Forecasting")
 
@@ -21,7 +24,7 @@ st.sidebar.markdown("Using the census data of 🇵🇰 - we predict the populati
 #loading the csv file
 #data_url = ("C:/Users/hp/Desktop/Python dashboards/streamlit/combined_districts.csv")
 
-DATA_URL = ("combined_districts.csv")
+DATA_URL = ('combined_districts.csv')
 
 @st.cache
 def load_data():
@@ -31,51 +34,64 @@ def load_data():
 df = load_data()
 
 
-#metrics =['district','children','teen','young_adult','adult','senior','total']
-#cols = st.selectbox('Population metric to view', metrics)
-# let's ask the user which column should be used as Index
-#if cols in metrics:   
- #   metric_to_show_in_pop_Layer = cols
-
-# Filters UI
-# subset_data = df
-# province_name_input = st.sidebar.multiselect(
-# 'Province',
-# df.groupby('province').count().reset_index()['province'].tolist())
-# # by country name
-# if len(province_name_input) > 0:
-#     subset_data = df[df['province'].isin(province_name_input)]
-
-# Create a list of possible values and multiselect menu with them in it.
-# provinces = df['province'].unique()
-# provinces_selected = st.sidebar.multiselect('Select provinces',provinces)
-
-# # Mask to filter dataframe
-# mask_provinces = df['province'].isin(provinces_selected)
-
-# option1 = df[mask_provinces]
-
 st.sidebar.markdown("### Predicted population per year")
 st.markdown("### Actual Population in different Years")
 sum = df.groupby(['year'])['Total'].sum().reset_index()
-fig = px.bar(sum, x='year', y='Total', color='year')
-st.plotly_chart(fig)
 
-#population_count = df.groupby(['province'])['Total'].agg('sum')
-#population_count = df.groupby(['province','district'])['Total'].sum()
-#population_count = df.groupby(['province'])['Total'].sum().reset_index()
-#population_count = df['province'].value_counts()
-#population_count = pd.DataFrame({'province':population_count.index, 'Total':population_count.values})
+#histogram or piechart
+select = st.sidebar.selectbox('Visualization Type', ['Histogram', 'Pie Chart'], key='1')
+if not st.sidebar.checkbox("Hide", True):
+    #st.markdown("## Population Predicition")
+    if select == "Histogram":
+        fig = px.bar(sum, x='year', y='Total', color='year')
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sum, values='Total', names='year')
+        st.plotly_chart(fig)
+
 option = st.sidebar.selectbox(
         'Please Select the Year you want prediction for',
         (2007, 2009, 2011, 2013, 2015))
+    
 
 option1 = st.sidebar.selectbox(
         'Please Select the Province you want prediction for',
         ("Sindh", "Balochistan", "Punjab", "NWFP"))
 
+option2 = st.sidebar.selectbox(
+        'Please Select the District you want prediction for',
+        ('Attock', 'Abbottabad', 'Awaran', 'Badin', 'Bahawalnagar',
+       'Bahawalpur', 'Bannu', 'Barkhan', 'Batagram', 'Bhakkar',
+       'BolanKachhi', 'Bonair', 'Chagai', 'Chakwal', 'Charsada',
+       'Chitral', 'D.I.Khan', 'D.G.Khan', 'Dadu',
+       'Dera_Bughti', 'Faisalabad', 'Ghotki', 'Gujranwala',
+       'Gujrat', 'Hafizabad', 'Hangu', 'Haripur', 'Hyderabad',
+       'Islamabad', 'Jacobabad', 'Jaffarabad', 'Jamshoro', 'Jehlum',
+       'Jhal Magsi', 'Jhang', 'Kalat', 'Karachi', 'Karak', 'Kashmore',
+       'Kasur', 'Khairpur', 'Khanewal', 'Khushab',
+       'Khuzdar', 'Kohat', 'Kohistan', 'Kohlu',
+       'Lahore', 'Lakki Marwat', 'Larkana', 'Lasbela', 'Layyah',
+       'Lodhran', 'Loralai', 'Lower Dir', 'Mianwali', 'Matiari',
+       'Malakand', 'Mandi Bahauddin', 'Mansehra', 'Mardan', 'Mastung',
+       'Mirpurkhas', 'Multan', 'Musa Khel', 'Muzaffargarh',
+       'Nankana Sahib', 'Narowal', 'Nasirabad', 'Naushahro Feroze',
+       'Nawabshah', 'Nowshera', 'Okara', 'Pakpattan',
+        'Pishin', 'Peshawar', 'Qilla Saifullah',
+       'Qilla Abdullah', 'Quetta', 'Rahim Yar Khan', 'Rajanpur',
+       'Rawalpindi', 'Sahiwal', 'Sanghar', 'Sarghodha', 'Shahdadkot',
+        'Shangla', 'Sheerani', 'Sheikhupura',
+       'Shikarpur', 'Sialkot', 'Sibbi', 'Sukkur', 'Swabi',
+       'Swat', 'T.T.Singh', 'Tando Allah Yar', 'Tando Mohammad Khan',
+       'Tank', 'Tharparkar', 'Thatta',
+       'Umer kot', 'Upper Dir', 'Vehari', 'Washuk', 'Ziarat'))
 
-#graph 2 
+
+#map
+map_data = DATA_URL
+st.map(df)
+
+
+#graph 
 st.subheader('Overall prediction of Pakistan By Our Model')
 #code for overall
 def model(year):
@@ -192,8 +208,32 @@ def model_province(year, province):
         print("dataset not present")
 
 
-model, t_pop_pro =  model_province(option, option1)
-fig = px.bar(model, x='Age Bracket', y='Population', color = 'Age Bracket')
+model_prov, t_pop_pro =  model_province(option, option1)
+fig = px.bar(model_prov, x='Age Bracket', y='Population', color = 'Age Bracket')
 predicted_year = option + 2
 st.write("The prediction is for Province "+ option1 + " for the Year " + str(predicted_year), fig)
 st.write("The prediction is for Province "+ option1 + " for the Year " + str(predicted_year), t_pop_pro)
+
+#graph 4
+st.subheader('Overall prediction of district By Our Model')
+
+def model_dist(year, district):
+    import os
+    path = "group_district/group_"+district+"_"+str(year)+".csv"
+    data = pd.read_csv(path)
+    pop_matrix = data.to_numpy()
+    path1 = "matrix_2015/matrix_"+district+"_2015.csv"
+    matrix = pd.read_csv(path1)
+    prob_matrix = matrix.to_numpy()
+    pred_matrix = pop_matrix.dot(prob_matrix)
+    df = pd.DataFrame(data = pred_matrix.transpose(), columns=["Population"])
+    df.insert(0, "Age Bracket", ["children", "teen", "young_adult", "adult", "senior"], True)
+    #df.to_csv('prediction/prediction_2015.csv', index=False)
+    return df, int(pred_matrix.sum())
+
+
+model_dist, t_pop_dist =  model_dist(option, option2)
+fig = px.bar(model_dist, x='Age Bracket', y='Population', color = 'Age Bracket')
+predicted_year = option + 2
+st.write("The prediction is for Province "+ option2 + " for the Year " + str(predicted_year), fig)
+st.write("The prediction is for Province "+ option2 + " for the Year " + str(predicted_year), t_pop_dist)
